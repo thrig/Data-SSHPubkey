aa649dd1908dd24488859fcc88294a3e7b8144ef
Jeremy Mates <jmates@cpan.org>
2019-02-27 20:01:02 -0800

    ... eh, ship it

--- lib/Data/SSHPubkey.pm
+++ lib/Data/SSHPubkey.pm
@@ -24,9 +24,54 @@ sub pubkeys {
     open $fh, '<', $input or croak "could not open $input: $!";
     my @keys;
     while ( my $line = readline $fh ) {
-; # TODO
+        if ( $line =~ m{^(-----BEGIN RSA PUBLIC KEY-----)} ) {
+            my $key = $1;
+            my ( $ok, $data ) = _until_end( $fh, '-----END RSA PUBLIC KEY-----' );
+            croak "could not parse PEM pubkey" unless $ok;
+            push @keys, [ 'PEM', $key . $/ . $data ];
+        } elsif ( $line =~ m{^(-----BEGIN PUBLIC KEY-----)} ) {
+            my $key = $1;
+            my ( $ok, $data ) = _until_end( $fh, '-----END PUBLIC KEY-----' );
+            croak "could not parse PKCS8 pubkey" unless $ok;
+            push @keys, [ 'PKCS8', $key . $/ . $data ];
+
+        } elsif ( $line =~ m{^(---- BEGIN SSH2 PUBLIC KEY ----)} ) {
+            my $key = $1;
+            my ( $ok, $data ) = _until_end( $fh, '---- END SSH2 PUBLIC KEY ----' );
+            croak "could not parse RFC4716 pubkey" unless $ok;
+            push @keys, [ 'RFC4716', $key . $/ . $data ];
+        } elsif (
+            # long enough for a RSA 4096-bit key, a bit too genereous
+            # for ed25519 so probably should instead be done for each
+            # key type
+            $line =~ m{
+              (ecdsa-sha2-nistp256|ssh-ed25519|ssh-rsa) [\t ]+?
+              ([A-Za-z0-9+/=]{64,717}) (?:[\t ]|$) }x
+        ) {
+            push @keys, [ $1, "$1 $2" ];
+        }
     }
     return \@keys;
+}
+
+# KLUGE this will very likely need changes depending on what clients in
+# the while send in with this format, as I really only deal with OpenSSH
+# type keys and not this form (e.g. skip Comment: or such fields?)
+sub _until_end {
+    my ( $fh, $fin ) = @_;
+    my $ok;
+    my $ret = '';
+    while ( my $line = readline $fh ) {
+        if ( $line =~ m/^($fin)/ ) {
+            $ret .= $1;
+            $ok = 1;
+            last;
+        }
+        if ( $line =~ m/^(.{1,80})$/ ) {    # TODO tighten this up...
+            $ret .= $1 . $/;
+        }
+    }
+    return $ok, $ret;
 }
 
 1;
@@ -41,26 +86,41 @@ Data::SSHPubkey - utility function to parse SSH public keys with
   use Data::SSHPubkey;
 
   my $keylist = Data::SSHPubkey::pubkeys( $file_or_scalarref );
-  ...
+  for my $ref ( @$keylist ) {
+      my ($type, $pubkey) = @$ref;
+      ...
+  }
 
 =head1 DESCRIPTION
 
-C<Data::SSHPubkey> parses SSH public keys, or at least those supported
-by C<ssh-keygen(1)>. It may be prudent to check any uploaded data with
-C<ssh-keygen> though this module should help extract that from web form
-upload data or the like to get to that step.
+C<Data::SSHPubkey> parses SSH public keys, or at least some of those
+supported by C<ssh-keygen(1)>. It may be prudent to check any uploaded
+data with C<ssh-keygen> though this module should help extract that from
+web form upload data or the like to get to that step.
 
-=head1 FUNCTIONS
+Currently supported public key types:
+
+  ecdsa ed25519 rsa
+  PEM PKCS8 RFC4716
+
+Neither SSH1 keys nor SSH2 DSA keys are supported.
+
+=head1 SUBROUTINE
 
 =over 4
 
 =item B<pubkeys> I<filename-or-scalarref>
 
-A filename (scalar) will be opened and the public keys therein parsed;
-a scalar reference will be treated as an in-memory file and will
-likewise be parsed.
+A filename (scalar) will be opened and the public keys therein parsed; a
+scalar reference will be treated as an in-memory file and will likewise
+be opened and parsed.
 
-TODO return format
+This routine will B<croak> on error as, in theory, all the errors should
+be due to the data passed in by the caller, or possibly the system has
+run out of memory, or something.
+
+The return format is a reference to a list of C<[ $type, $pubkey ]>
+lists.
 
 =back
 
@@ -76,10 +136,22 @@ Patches might best be applied towards:
 
 L<https://github.com/thrig/Data-SSHPubkey>
 
+=head2 Known Issues
+
+Probably not enough guards or checks against hostile input (too much
+data, etc).
+
+Support for the
+
+  PEM PKCS8 RFC4716
+
+key types is pretty weak and needs improvement.
+
 =head1 SEE ALSO
 
 L<Config::OpenSSH::Authkey> - older module more aimed at management of
-C<~/.ssh/authorized_keys> data and not specifically public keys.
+C<~/.ssh/authorized_keys> data and not specifically public keys. It does
+have support for SSH2 DSA or SSH1 keys, though.
 
 =head1 AUTHOR
 
--- t/10-pubkeys.t
+++ t/10-pubkeys.t
@@ -9,19 +9,28 @@ use Data::SSHPubkey;
 
 can_ok( 'Data::SSHPubkey', qw(pubkeys) );
 
-# these are from ssh-keygen -t option types, and the rsa thereof also
-# converted into the three support -m option types listed by SSH as
-# shipped with OpenBSD 6.4. not supported are SSH2 DSA keys nor the much
-# older SSH1 protocol keys
 my @keyfiles =
   (qw(PEM.pub PKCS8.pub RFC4716.pub ecdsa.pub ed25519.pub rsa.pub));
+
+my %types = (
+    ecdsa   => { type => 'ecdsa-sha2-nistp256', keyre => qr{^ecdsa-sha2-nistp256} },
+    ed25519 => { type => 'ssh-ed25519',         keyre => qr{^ssh-ed25519} },
+    rsa     => { type => 'ssh-rsa',             keyre => qr{^ssh-rsa} },
+    PEM     => { type => 'PEM',     keyre => qr{^-----BEGIN RSA PUBLIC KEY-----} },
+    PKCS8   => { type => 'PKCS8',   keyre => qr{^-----BEGIN PUBLIC KEY-----} },
+    RFC4716 => { type => 'RFC4716', keyre => qr{^---- BEGIN SSH2 PUBLIC KEY ----} },
+);
 
 for my $f (@keyfiles) {
     ( my $type = $f ) =~ s/\.pub//;
     my $ret = Data::SSHPubkey::pubkeys( File::Spec->catfile( 't', $f ) );
-    use Data::Dumper;
-    diag Dumper $ret;
-    ok( defined $ret, "keyfile $f" );
+    is( scalar @$ret, 1, "only one key in $f" );
+    my ( $parse_type, $data ) = @{ $ret->[0] };
+    is( $parse_type, $types{$type}->{type}, "bad type for $f" );
+    ok( $data =~ m/$types{$type}->{keyre}/, "pub key data for $f" );
 }
 
-plan tests => 1 + scalar @keyfiles
+# TODO probably should check input that has two, or possibly maybe even
+# three keys in it, but we don't want to get too crazy here...
+
+plan tests => 1 + 3 * scalar @keyfiles
